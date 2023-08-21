31150dbde71ee5c5b9c8c8731887bc0784b42102
Vinicius Santiago <61833324+Santiaago-vinicius@users.noreply.github.com>
2023-08-21 12:19:08 -0300

    Add files via upload

---
+++ ATIVIDADE2/q7.ts
@@ -0,0 +1,9 @@
+const name = "Ely";
+const paymentAmount = 120.56;
+const language = "TypeScript";
+
+
+console.log(`${name}
+My payment time is ${paymentAmount}
+and
+my preferred language is ${language}`);
