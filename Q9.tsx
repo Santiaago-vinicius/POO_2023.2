99b72bb6b7bab9fe4b13f0d64de2245b31b557db
Vinicius Santiago <61833324+Santiaago-vinicius@users.noreply.github.com>
2023-10-07 17:13:43 -0300

    Q9.tsx

--- Q9.tsx
+++ Q9.tsx
@@ -1,25 +1,14 @@
-class SituacaoFinanceira {
-    valorCreditos: number;
-    valorDebitos: number;
+class SituacaoFinanceira{
+    valorCreditos : number = 0
+    valorDebito : number = 0
 
-    constructor(valorCreditos: number, valorDebitos: number) {
-        this.valorCreditos = valorCreditos;
-        this.valorDebitos = valorDebitos;
-    }
-
-    saldo(): number {
-        return this.valorCreditos - this.valorDebitos;
+    saldo(): number{
+        return this.valorCreditos - this.valorDebito
     }
 }
 
+let situcaofinanceira : SituacaoFinanceira = new SituacaoFinanceira()
+situcaofinanceira.valorCreditos = 450
+situcaofinanceira.valorDebito = 200
 
-const situacao = new SituacaoFinanceira(1500, 800); 
-
-
-const saldoAtual = situacao.saldo();
-console.log(`Saldo atual: ${saldoAtual}`);
-
-
-// To learn more about the language, click above in "Examples" or "What's New".
-// Otherwise, get started by removing these comments and the world is your playground.
-  
+console.log(situcaofinanceira.saldo())
