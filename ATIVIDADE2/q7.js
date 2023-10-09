aa4d2c507d2fde5e649553568036aab93cc6863e
Vinicius Santiago <61833324+Santiaago-vinicius@users.noreply.github.com>
2023-10-09 09:32:30 -0300

    Delete ATIVIDADE2 directory

--- ATIVIDADE2/q7.js
+++
@@ -1,4 +0,0 @@
-var name = "Ely";
-var paymentAmount = 120.56;
-var language = "TypeScript";
-console.log("".concat(name, "\nMy payment time is ").concat(paymentAmount, "\nand\nmy preferred language is ").concat(language));
--- ATIVIDADE2/q7.ts
+++
@@ -1,9 +0,0 @@
-const name = "Ely";
-const paymentAmount = 120.56;
-const language = "TypeScript";
-
-
-console.log(`${name}
-My payment time is ${paymentAmount}
-and
-my preferred language is ${language}`);
