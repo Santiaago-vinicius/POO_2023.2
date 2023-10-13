e5941efb7a7ac55b7b331f25facc87edc477d886
Vinicius Santiago <61833324+Santiaago-vinicius@users.noreply.github.com>
2023-10-13 10:22:10 -0300

    Add files via upload

---
+++ Atividade_2/q7.js
@@ -0,0 +1,4 @@
+var nom = 'Ely';
+var pagamento = 120.56;
+var linguagem = 'typescript';
+console.log("".concat(nom, "\nMy payment time is ").concat(pagamento, "\nand\nmy preffered language is TypeScript"));
---
+++ Atividade_2/q7.ts
@@ -0,0 +1,8 @@
+let nom : string = 'Ely'
+let pagamento: number = 120.56
+let linguagem: string = 'typescript'
+
+console.log(`${nom}
+My payment time is ${pagamento}
+and
+my preffered language is TypeScript`)
